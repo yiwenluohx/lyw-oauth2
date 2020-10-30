aff8172380dfbdb63c6c3066c387ba4543d1fffe
luohx <Lhongxiao@lcchain.com>
2020-10-30 19:17:56 +0800

    添加注释

--- lyw-oauth2-authorization-code/src/main/java/com/lyw/cloud/oauth2/authorization/code/LywOauth2AuthorizationCodeApplication.java
+++ lyw-oauth2-authorization-code/src/main/java/com/lyw/cloud/oauth2/authorization/code/LywOauth2AuthorizationCodeApplication.java
@@ -2,11 +2,16 @@ package com.lyw.cloud.oauth2.authorization.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class LywOauth2AuthorizationCodeApplication {
 
     public static void main(String[] args) {
+        BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
+        String cms = bCrypt.encode("{noop}cms");
+        String sc = bCrypt.encode("{noop}secret");
+        String mm = bCrypt.encode("123");
         SpringApplication.run(LywOauth2AuthorizationCodeApplication.class, args);
     }
 
